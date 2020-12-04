8565ac43af2b7da3581c3d823e0ac9de1b9a9887
drb <266983@viauc.dk>
2020-12-04 16:06:34 +0100

    Cleanup

--- DataContext/Models/FlightDc.cs
+++ DataContext/Models/FlightDc.cs
@@ -24,18 +24,5 @@ namespace FlightsWebApplication.Models
         public int Distance { get; set; }
         public int? Hour { get; set; }
         public int? Minute { get; set; }
-        //public DateTime Date { get; set; }
-        //public DateTime Time { get; set; }
-        //public int DepartureTime { get; set; }
-        //public int DepartureDelay { get; set; }
-        //public int ArrivalTime { get; set; }
-        //public int ArrivalDelay { get; set; }
-        //public string Carrier { get; set; }
-        //public string TailNumber { get; set; }
-        //public int Fligth { get; set; }
-        //public string Origin { get; set; }
-        //public string Destination { get; set; }
-        //public int AirTime { get; set; }
-        //public int Distance { get; set; }
     }
 }
