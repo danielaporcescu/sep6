7d8e22b53c0076eb2a71d5adba2807c16caada3f
drb <266983@viauc.dk>
2020-12-08 13:02:03 +0100

    Added copy method + ordering by Month

--- Services/Models/Flights/FlightsFromOriginsPerMonth.cs
+++ Services/Models/Flights/FlightsFromOriginsPerMonth.cs
@@ -6,5 +6,10 @@
         public double EWR { get; set; } = 0;
         public double JFK { get; set; } = 0;
         public double LGA { get; set; } = 0;
+
+        public FlightsFromOriginsPerMonth Copy()
+        {
+            return new FlightsFromOriginsPerMonth() { Month = this.Month, EWR = this.EWR, JFK = this.JFK, LGA = this.LGA };
+        }
     }
 }
