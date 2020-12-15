131ee9ad8f11171463bc8ef275387ddc952c120b
drb <266983@viauc.dk>
2020-12-15 19:04:46 +0100

    Setup, airline repo test, flight repot test

---
+++ ServicesTest/BaseDbTest.cs
@@ -0,0 +1,19 @@
+﻿using FlightsWebApplication;
+
+namespace ServicesTest
+{
+    public class BaseDbTest
+    {
+        protected readonly TestWebApplicationFactory<Startup> factory;
+
+        public BaseDbTest()
+        {
+            this.factory = new TestWebApplicationFactory<Startup>();
+        }
+
+        public void Dispose()
+        {
+            factory.Dispose();
+        }
+    }
+}
