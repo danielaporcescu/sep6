467518f9a2e04ee2ccada2cf0a1d40791840e272
drb <266983@viauc.dk>
2020-12-16 11:59:08 +0100

    REMOVED REPLACEMENT OF UAA CONTEXT IN STARTUP

--- ServicesTest/Internal/TestWebApplicationFactory.cs
+++ ServicesTest/Internal/TestWebApplicationFactory.cs
@@ -1,11 +1,8 @@
 ﻿using DataContext.Context;
-using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.Data.Sqlite;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
 using System.Data.Common;
-using System.Linq;
 
 namespace ServicesTest
 {
@@ -29,36 +26,40 @@ namespace ServicesTest
 
         public UAAContext GetUAAContext()
         {
-            var scopeFactory = Services.GetService<IServiceScopeFactory>();
-            var scope = scopeFactory.CreateScope();
+            var context = new UAAContext(new DbContextOptionsBuilder<UAAContext>().UseSqlite(connection).Options);
+            context.Database.EnsureCreated();
 
-            return scope.ServiceProvider.GetService<UAAContext>();
+            return context;
+            //var scopeFactory = Services.GetService<IServiceScopeFactory>();
+            //var scope = scopeFactory.CreateScope();
+
+            //return scope.ServiceProvider.GetService<UAAContext>();
         }
 
-        protected override void ConfigureWebHost(IWebHostBuilder builder)
-        {
-            builder.ConfigureServices(services =>
-            {
-                var descriptor = services.SingleOrDefault(
-                    d => d.ServiceType == typeof(DbContextOptions<UAAContext>));
-                services.Remove(descriptor);
+        //protected override void ConfigureWebHost(IWebHostBuilder builder)
+        //{
+        //    builder.ConfigureServices(services =>
+        //    {
+        //        var descriptor = services.SingleOrDefault(
+        //            d => d.ServiceType == typeof(DbContextOptions<UAAContext>));
+        //        services.Remove(descriptor);
 
-                services.AddDbContext<UAAContext>(options =>
-                {
-                    options.UseSqlite(connection);
-                });
-                // Init/Create db
-                InitializeDb(services.BuildServiceProvider());
-            });
-        }
+        //        services.AddDbContext<UAAContext>(options =>
+        //        {
+        //            options.UseSqlite(connection);
+        //        });
+        //        // Init/Create db
+        //        InitializeDb(services.BuildServiceProvider());
+        //    });
+        //}
 
-        private void InitializeDb(ServiceProvider serviceProvider)
-        {
-            using var scope = serviceProvider.CreateScope();
-            var database = scope.ServiceProvider.GetRequiredService<UAAContext>();
+        //private void InitializeDb(ServiceProvider serviceProvider)
+        //{
+        //    using var scope = serviceProvider.CreateScope();
+        //    var database = scope.ServiceProvider.GetRequiredService<UAAContext>();
 
-            database.Database.EnsureCreated();
-            database.SaveChanges();
-        }
+        //    database.Database.EnsureCreated();
+        //    database.SaveChanges();
+        //}
     }
 }
